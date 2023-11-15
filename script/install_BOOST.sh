53847cb9460c41e172cb6f4b15ba82fe332641a3
absingh <absingh@mpi-cbg.de>
2023-11-15 15:56:06 +0100

    update boost to 18300

--- script/install_BOOST.sh
+++ script/install_BOOST.sh
@@ -14,10 +14,10 @@ if [ -d "$1/BOOST" ]; then
   exit 0
 fi
 
-rm boost_1_82_0.tar.bz2
-wget http://ppmcore.mpi-cbg.de/upload/boost_1_82_0.tar.bz2
-tar -xf boost_1_82_0.tar.bz2
-cd boost_1_82_0
+rm boost_1_83_0.tar.bz2
+wget https://boostorg.jfrog.io/artifactory/main/release/1.83.0/source/boost_1_83_0.tar.gz
+tar -xf boost_1_83_0.tar.bz2
+cd boost_1_83_0
 if [ x"$4" != x"" ]; then
 	if [ -f $HOME/user-config.jam ]; then
 		mv $HOME/user-config.jam $HOME/user-config.jam_bck
@@ -28,7 +28,7 @@ if [ x"$4" != x"" ]; then
 		echo "using gcc : : $4 ; " > $HOME/user-config.jam
 	fi
 fi
-
+#using gcc : 13 : g++-13 : <linkflags>"-Wl,-ld_classic" ;
 ## When we are on powerPC we avoid booststrap for some reason will try xlcpp and most probably will fail we fall back to gcc
 
 if [ x"$arch" == x"ppc64le" ]; then
@@ -38,7 +38,7 @@ if [ x"$arch" == x"ppc64le" ]; then
 	cp tools/build/src/engine/b2 .
 	cp tools/build/src/engine/bjam .
 else
-	./bootstrap.sh --with-toolset=$3
+	./bootstrap.sh --with-toolset=$CC
 fi
 
 mkdir $1/BOOST
@@ -54,17 +54,17 @@ if [ x"$platform" == x"osx" ]; then
       if [ x"$3" == x"" ]; then
           ./b2 -a -j $2 install --prefix=$1/BOOST address-model=64 architecture=x86 abi=sysv binary-format=mach-o toolset=clang  -sNO_LZMA=1 -sNO_ZSTD=1
         else
-          ./b2 -a -j $2 install --prefix=$1/BOOST address-model=64 architecture=x86 abi=sysv binary-format=mach-o toolset=gcc  -sNO_LZMA=1 -sNO_ZSTD=1
+          ./b2 -a -j $2 install --prefix=$1/BOOST address-model=64 architecture=x86 abi=sysv binary-format=mach-o toolset=$CC  -sNO_LZMA=1 -sNO_ZSTD=1
       fi
     fi
 else
     ./b2 -a -j $2 install --prefix=$1/BOOST  -sNO_LZMA=1 -sNO_ZSTD=1
 fi
 
-rm -rf boost_1_82_0
+rm -rf boost_1_83_0
 
 if [ -f $HOME/user-config.jam_bck ]; then
 	mv $HOME/user-config.jam_bck $HOME/user-config.jam
 fi
-rm -rf boost_1_82_0.tar.bz2
+rm -rf boost_1_83_0.tar.bz2
 
